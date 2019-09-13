20a383c5e1c9c65ffe50d74b304249e28d2d063a
Georgi Markov <markov_ood@abv.bg>
2019-09-13 05:47:30 +0300

    Completed Auto repair and service task but not confirmed with Judge as it is missing from there...

---
+++ StacksAndQueues - Exercise/AutoRepairAndService/AutoRepairAndService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoRepairAndService
+{
+    public class AutoRepairAndService
+    {
+        public static void Main()
+        {
+            Queue<string> awaitingCars = new Queue<string>(
+                                            Console.ReadLine()
+                                                .Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            Stack<string> servedCars = new Stack<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+
+                if (command == "Service")
+                {
+                    string car = awaitingCars.Dequeue();
+                    Console.WriteLine($"Vehicle {car} got served.");
+                    servedCars.Push(car);
+                }
+                else if (command == "History")
+                {
+                    Console.WriteLine(string.Join(", ", servedCars));
+                }
+                else
+                {
+                    // CarInfo-{modelName}
+                    string modelName = command.Split('-')[1];
+                    if (servedCars.Contains(modelName))
+                    {
+                        Console.WriteLine("Served.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Still waiting for service.");
+                    }
+                }
+            }
+
+            if (awaitingCars.Count > 0)
+            {
+                Console.WriteLine($"Vehicles for service: {string.Join(", ", awaitingCars)}");
+            }
+
+            Console.WriteLine($"Served vehicles: {string.Join(", ", servedCars)}");
+        }
+    }
+}
