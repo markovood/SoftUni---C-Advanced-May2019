fb094e4374b888c836740af7d64d84b8a600781c
Georgi Markov <markov_ood@abv.bg>
2019-09-20 01:01:36 +0300

    Completed Count symbols task...

---
+++ SetsAndDictionaries - Exercise/CountSymbols/CountSymbols.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> symbolsAndOccurences = new SortedDictionary<char, int>();
+            foreach (var symbol in text)
+            {
+                if (symbolsAndOccurences.ContainsKey(symbol))
+                {
+                    symbolsAndOccurences[symbol]++;
+                }
+                else
+                {
+                    symbolsAndOccurences.Add(symbol, 1);
+                }
+            }
+
+            // Print result
+            foreach (var item in symbolsAndOccurences)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
