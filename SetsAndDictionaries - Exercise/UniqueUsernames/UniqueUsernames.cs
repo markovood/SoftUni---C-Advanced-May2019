3e88f22f5e32553fcd0cd7a5eceebb0fc3309ab0
Georgi Markov <markov_ood@abv.bg>
2019-09-20 00:53:41 +0300

    Completed Unique usernames task...

---
+++ SetsAndDictionaries - Exercise/UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            int usernamesCount = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+            for (int i = 0; i < usernamesCount; i++)
+            {
+                string userName = Console.ReadLine();
+                uniqueUsernames.Add(userName);
+            }
+
+            foreach (var username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
