42db9cb24be2bbcf3c2a34b1169cd3c687479916
Georgi Markov <markov_ood@abv.bg>
2019-09-20 00:57:37 +0300

    Completed Periodic table task...

---
+++ SetsAndDictionaries - Exercise/PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    public class PeriodicTable
+    {
+        public static void Main()
+        {
+            int compoundsCount = int.Parse(Console.ReadLine());
+
+            SortedSet<string> containingElements = new SortedSet<string>();
+            for (int i = 0; i < compoundsCount; i++)
+            {
+                string[] compoundElements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var element in compoundElements)
+                {
+                    containingElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', containingElements));
+        }
+    }
+}
