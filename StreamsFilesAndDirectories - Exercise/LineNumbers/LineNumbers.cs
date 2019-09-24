48ef6d476c59b0832ad91d04a2bddcb741df51ab
Georgi Markov <markov_ood@abv.bg>
2019-09-25 00:37:01 +0300

    Completed Line numbers task...

---
+++ StreamsFilesAndDirectories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            string pathIn = @"..\..\..\..\Resources\text.txt";
+            string pathOut = @"..\..\..\output.txt";
+
+            string[] lines = File.ReadAllLines(pathIn);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = $"Line {i + 1}: {lines[i]} ({CountLetters(lines[i])})({CountPunctuationMarks(lines[i])})";
+            }
+
+            File.WriteAllLines(pathOut, lines);
+        }
+
+        private static int CountPunctuationMarks(string line)
+        {
+            char[] symbols = line.ToCharArray();
+            int count = 0;
+            foreach (var symbol in symbols)
+            {
+                if (char.IsPunctuation(symbol))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static int CountLetters(string line)
+        {
+            char[] symbols = line.ToCharArray();
+            int count = 0;
+            foreach (var symbol in symbols)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
