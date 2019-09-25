435e962cda4eafc9a02d577fce181a10dd47a22f
Georgi Markov <markov_ood@abv.bg>
2019-09-26 00:15:30 +0300

    Completed Copy binary file...

---
+++ StreamsFilesAndDirectories - Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            char dirSeparator = Path.DirectorySeparatorChar;
+            string fileNameToCopy = "copyMe.png";
+            string copiedFileName = "copyMe - copy.png";
+            string pathIn = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}{fileNameToCopy}";
+            string pathOut = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}{copiedFileName}";
+
+            using (var reader = new FileStream(pathIn, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var writer = new FileStream(pathOut, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    int readBytes = reader.Read(buffer, 0, buffer.Length);
+
+                    while (readBytes != 0)
+                    {
+                        writer.Write(buffer, 0, readBytes);
+
+                        readBytes = reader.Read(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
