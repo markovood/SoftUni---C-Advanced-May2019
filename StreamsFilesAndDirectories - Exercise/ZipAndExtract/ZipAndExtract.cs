6ebccf9ee6dc1bf4bf855b79c660647617e44d76
Georgi Markov <markov_ood@abv.bg>
2019-09-26 02:04:46 +0300

    Completed Zip and Extract task...

---
+++ StreamsFilesAndDirectories - Exercise/ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    public class ZipAndExtract
+    {
+        public static void Main()
+        {
+            string filePath = @"..\..\..\..\Resources\copyMe.png";
+            string archivePath = @"..\..\..\copyMe.zip";
+
+            // before uncomment the 'un-archiving' lines, comment first the 'archiving' ones or exception will
+            // be thrown
+
+            // Archiving
+            using (ZipArchive archive = ZipFile.Open(archivePath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+
+            // un-archiving
+            // ZipFile.ExtractToDirectory(archivePath, @"..\..\..\");
+        }
+    }
+}
