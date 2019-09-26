98d622f9a208e54800f3a1ad93fb76410e60c281
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:56:45 +0300

    Completed Predicate for names task...

---
+++ FunctionalProgramming - Excercise/PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> predicate = n => n.Length <= nameLength;
+            Func<string[], string[]> filterFunc = collection => collection.Where(predicate).ToArray();
+            Action<string[]> printAction = collection => Console.WriteLine(string.Join(Environment.NewLine, collection));
+
+            names = filterFunc(names);
+            printAction(names);
+        }
+    }
+}
