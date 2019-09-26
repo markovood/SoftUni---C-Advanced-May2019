5914b107b3965c4d4b7117e6d04398402d38cea9
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:39:00 +0300

    Completed Action print task...

---
+++ FunctionalProgramming - Excercise/ActionPrint/ActionPrint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ActionPrint
+{
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            string[] strs = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> print = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+            print(strs);
+        }
+    }
+}
