8f8b1e070da5a330d405768a66ed3c23bd986c99
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:45:35 +0300

    Completed Custom min function task...

---
+++ FunctionalProgramming - Excercise/CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            int[] ints = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> min = Min;
+
+            Console.WriteLine(min(ints));
+        }
+
+        private static int Min(int[] nums)
+        {
+            int smallest = int.MaxValue;
+            foreach (var num in nums)
+            {
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+            }
+
+            return smallest;
+        }
+    }
+}
