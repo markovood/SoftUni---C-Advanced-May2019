872f8ec2091ea91d6766b74e68ee50d6d39b0247
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:54:03 +0300

    Completed Reverse and exclude task...

---
+++ FunctionalProgramming - Excercise/ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    public class ReverseAndExclude
+    {
+        public static void Main()
+        {
+            List<int> collection = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Action<List<int>> reverseAction = nums => nums.Reverse();
+            Func<List<int>, List<int>> removeFunc = nums => nums.Where(num => num % n != 0).ToList();
+            Action<List<int>> printAction = nums => Console.WriteLine(string.Join(' ', nums));
+
+            reverseAction(collection);
+            collection = removeFunc(collection);
+            printAction(collection);
+        }
+    }
+}
