4471bb38c5c96f8e26d6ffa74bf2d69b56353fa2
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:42:43 +0300

    Completed Knights of honor task...

---
+++ FunctionalProgramming - Excercise/KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    public class KnightsOfHonor
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => "Sir " + n)
+                .ToArray();
+
+            Action<string[]> print = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            print(names);
+        }
+    }
+}
