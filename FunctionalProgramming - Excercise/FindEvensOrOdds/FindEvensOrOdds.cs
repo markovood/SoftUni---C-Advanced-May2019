8c00f79b2a511321ef9d07ce1a66a20342a13f37
Georgi Markov <markov_ood@abv.bg>
2019-09-26 16:48:57 +0300

    Completed Find evens or odds...

---
+++ FunctionalProgramming - Excercise/FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+    public class FindEvensOrOdds
+    {
+        public static void Main()
+        {
+            string[] boundaries = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int lowerBound = int.Parse(boundaries[0]);
+            int upperBound = int.Parse(boundaries[1]);
+
+            string filter = Console.ReadLine();
+
+            int[] range = Enumerable.Range(lowerBound, upperBound - lowerBound + 1).ToArray();
+
+            Predicate<int> isEven = x => x % 2 == 0;
+            Predicate<int> isOdd = x => x % 2 != 0;
+
+            List<int> result = new List<int>();
+            foreach (var number in range)
+            {
+                if (filter == "even" ? isEven(number) : isOdd(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
