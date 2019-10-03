d030c1a16879dcb3faea8ebc341cc3d41fce1cf5
Georgi Markov <markov_ood@abv.bg>
2019-10-04 01:03:32 +0300

    Completed Custom stack task...

---
+++ Workshop - Lab/CustomStructures/MyStack.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStructures
+{
+    /// <summary>
+    /// Represents an Array that can store integer numbers, similar to the C# Stack but with custom functionality
+    /// </summary>
+    public class MyStack
+    {
+        private int capacity = 4;
+        private int[] array;
+
+        public MyStack()
+        {
+            this.array = new int[this.capacity];
+        }
+
+        public MyStack(int capacity)
+        {
+            this.capacity = capacity;
+            this.array = new int[this.capacity];
+        }
+
+        public int Count { get; private set; } = 0;
+
+        /// <summary>
+        /// Adds an element to the end of the collection
+        /// </summary>
+        /// <param name="element">The element to be added to the end of collection</param>
+        public void Push(int element)
+        {
+            // ensure capacity
+            if (this.Count == this.capacity)
+            {
+                Grow();
+            }
+
+            this.array[this.Count] = element;
+            this.Count++;
+        }
+
+        /// <summary>
+        /// Removes the last element form the collection
+        /// </summary>
+        /// <returns>Returns the removed element</returns>
+        public int Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("MyStack is empty!");
+            }
+
+            int removed = this.array[this.Count - 1];
+            this.array[this.Count - 1] = 0;// this line might be removed
+            this.Count--;
+            if (this.capacity / 2 > this.Count)
+            {
+                Shrink();
+            }
+
+            return removed;
+        }
+
+        /// <summary>
+        /// Returns the last element from the collection, but it doesn't remove it
+        /// </summary>
+        /// <returns>The last element from the collection</returns>
+        public int Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("MyStack is empty!");
+            }
+
+            int lastElement = this.array[this.Count - 1];
+            return lastElement;
+        }
+
+        /// <summary>
+        /// This goes through every element from the collection and executes the given action
+        /// </summary>
+        /// <param name="action">The action delegate to be executed over each element</param>
+        public void ForEach(Action<int> action)
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                action(this.array[i]);
+            }
+        }
+
+        private void Shrink()
+        {
+            // decrease the internal collection's length twice
+            this.capacity /= 2;
+            int[] newArray = new int[this.capacity];
+            for (int i = 0; i < this.Count; i++)
+            {
+                newArray[i] = this.array[i];
+            }
+
+            this.array = newArray;
+        }
+
+        private void Grow()
+        {
+            // increase the internal collection's length twice
+            this.capacity *= 2;
+            int[] newArray = new int[this.capacity];
+            this.array.CopyTo(newArray, 0);
+            this.array = newArray;
+        }
+    }
+}
--- Workshop - Lab/CustomStructures/StartUp.cs
+++ Workshop - Lab/CustomStructures/StartUp.cs
@@ -6,7 +6,7 @@ namespace CustomStructures
     {
         public static void Main()
         {
-            var list = new MyList(9);
+            MyList list = new MyList(9);
             list.Add(1);
             list.Add(2);
             list.Add(3);
@@ -14,11 +14,25 @@ namespace CustomStructures
             list.Add(5);
             int removed3 = list.RemoveAt(2);
             int removed4 = list.RemoveAt(2);
+            // int exceptionExpected = list.RemoveAt(20);
+            // int exceptionExpected = list.RemoveAt(-2);
             bool contains3 = list.Contains(3);
             bool contains5 = list.Contains(5);
             list.Swap(0, 2);
             list.Swap(2, 1);
             Console.WriteLine(list);
+
+            MyStack stack = new MyStack(9);
+            stack.Push(11);
+            stack.Push(22);
+            stack.Push(33);
+            stack.ForEach(Console.WriteLine);
+            int removed33 = stack.Pop();
+            int removed22 = stack.Pop();
+            int peeked = stack.Peek();
+            int removed11 = stack.Pop();
+            // int exceptionExpected = stack.Pop();
+            // int exceptionExpected = stack.Peek();
         }
     }
 }
