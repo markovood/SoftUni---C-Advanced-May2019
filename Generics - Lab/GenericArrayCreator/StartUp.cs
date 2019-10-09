43fd6bd0c46bcdb9cf970ef731b496d525861a84
Georgi Markov <markov_ood@abv.bg>
2019-10-10 01:06:41 +0300

    Completed Generics Lab...

---
+++ Generics - Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+    }
+}
