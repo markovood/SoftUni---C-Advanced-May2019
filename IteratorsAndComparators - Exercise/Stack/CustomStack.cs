5d5b53f24ef7b0fa6316143d95a6f28247767833
Georgi Markov <markov_ood@abv.bg>
2019-10-15 00:36:39 +0300

    Completed Custom stack task...

---
+++ IteratorsAndComparators - Exercise/Stack/CustomStack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private int capacity = 4;
+        private T[] array;
+
+        public CustomStack()
+        {
+            this.array = new T[this.capacity];
+        }
+
+        public CustomStack(int capacity)
+        {
+            this.capacity = capacity;
+            this.array = new T[this.capacity];
+        }
+
+        public int Count { get; private set; } = 0;
+
+        /// <summary>
+        /// Adds an element to the end of the collection
+        /// </summary>
+        /// <param name="element">The element to be added to the end of collection</param>
+        public void Push(T element)
+        {
+            // ensure capacity
+            if (this.Count == this.capacity)
+            {
+                Grow();
+            }
+
+            this.array[this.Count] = element;
+            this.Count++;
+        }
+
+        /// <summary>
+        /// Removes the last element form the collection
+        /// </summary>
+        /// <returns>Returns the removed element</returns>
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+
+            T removed = this.array[this.Count - 1];
+            this.array[this.Count - 1] = default(T);
+            this.Count--;
+            if (this.capacity / 2 > this.Count)
+            {
+                Shrink();
+            }
+
+            return removed;
+        }
+
+        private void Shrink()
+        {
+            // decrease the internal collection's length twice
+            this.capacity /= 2;
+            T[] newArray = new T[this.capacity];
+            for (int i = 0; i < this.Count; i++)
+            {
+                newArray[i] = this.array[i];
+            }
+
+            this.array = newArray;
+        }
+
+        private void Grow()
+        {
+            // increase the internal collection's length twice
+            this.capacity *= 2;
+            T[] newArray = new T[this.capacity];
+            this.array.CopyTo(newArray, 0);
+            this.array = newArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var stack = new CustomStack<int>();
+            while (true)
+            {
+                string[] commandTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (commandTokens[0] == "END")
+                {
+                    break;
+                }
+
+                try
+                {                    
+                    switch (commandTokens[0])
+                    {
+                        case "Push":
+                            var ints = commandTokens.Skip(1).Select(s => int.Parse(s.TrimEnd(','))).ToArray();
+                            foreach (var @int in ints)
+                            {
+                                stack.Push(@int);
+                            }
+
+                            break;
+                        case "Pop":
+                            stack.Pop();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message == "CustomStack is empty!")
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
