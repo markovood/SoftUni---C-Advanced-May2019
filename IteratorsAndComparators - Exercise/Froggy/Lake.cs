33cce134bf74b082a30045171a92f6712ba9e042
Georgi Markov <markov_ood@abv.bg>
2019-10-15 15:54:12 +0300

    Completed Froggy task...

---
+++ IteratorsAndComparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        private int currentIndex;
+
+        public Lake(int[] stones)
+        {
+            this.currentIndex = 0;
+            this.stones = stones;
+        }
+        
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i+=2)
+            {
+                yield return stones[i];
+            }
+
+            if (stones.Length % 2 == 0)
+            {
+                for (int i = stones.Length - 1; i >= 0; i -= 2)
+                {
+                    yield return stones[i];
+                }
+            }
+            else
+            {
+                for (int i = stones.Length - 2; i >= 0; i -= 2)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(stones);
+            List<int> frogsPath = new List<int>();
+            foreach (var stone in lake)
+            {
+                frogsPath.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", frogsPath));
+        }
+    }
+}
