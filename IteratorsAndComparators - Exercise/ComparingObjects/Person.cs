07f734c19542a707c1071922567b6b92615d6510
Georgi Markov <markov_ood@abv.bg>
2019-10-16 00:57:44 +0300

    Completed Comparing objects task...

---
+++ IteratorsAndComparators - Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person :IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Town { get; private set; }
+
+        public int CompareTo(Person other)
+        {
+            int equality = this.Name.CompareTo(other.Name);
+            if (equality == 0)
+            {
+                equality = this.Age.CompareTo(other.Age);
+                if (equality == 0)
+                {
+                    equality = this.Town.CompareTo(other.Town);
+                    return equality;
+                }
+                else
+                {
+                    return equality;
+                }
+            }
+            else
+            {
+                return equality;
+            }
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/ComparingObjects/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // "{name} {age} {town}"
+            var persons = new List<Person>();
+            while (true)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (personInfo[0] == "END")
+                {
+                    break;
+                }
+
+                var person = new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+                persons.Add(person);
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            var personToCompare = persons[n - 1];
+            persons.RemoveAt(n - 1);
+
+            int same = 1;
+            int different = 0;
+            foreach (var person in persons)
+            {
+                if (personToCompare.CompareTo(person) == 0)
+                {
+                    same++;
+                }
+                else
+                {
+                    different++;
+                }
+            }
+
+            if (same == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{same} {different} {persons.Count + 1}");
+            }
+        }
+    }
+}
